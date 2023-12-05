044a2233bc71075a6d553e929418f591e7f68aa7
phong <tpcreative.co@gmail.com>
2023-12-05 16:26:35 +0700

    Added zxing Added pods file Added sample qrcode Added tabs Added package Added scanner Added navigation link Added icon app Added details view Added generate view Added preview view Added save qrcode Added settings page Designed history list

--- qrscanner/ui/history/HistoryView.swift
+++ qrscanner/ui/history/HistoryView.swift
@@ -7,23 +7,69 @@
 
 import Foundation
 import SwiftUI
-
+import NavigationSearchBar
 struct HistoryView : View {
-    @State var image : CGImage?
-    
+    @State private var search = ""
+    @State var scopeSelection: Int = 0
     var body : some View {
-        VStack {
-            if let cgImage = image {
-                Image(uiImage: UIImage(cgImage: cgImage))
-                    .imageScale(.large)
-                    .foregroundStyle(.tint)
+        NavigationView {
+            VStack{
+                List {
+                    Text("Qr code")
+                    Text("Bar code")
+                }
             }
-            
-            Text("Hello, world!")
-        }
-        .padding()
-        .onAppear(){
-            image = QRWriter().writeQRCode("Hello")
+            .navigationTitle("History")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItemGroup(placement: .topBarLeading) {
+                    Button {
+                        
+                    }label: {
+                        Image(systemName: "square.and.arrow.up")
+                    }
+                    
+                    Button {
+                        
+                    }label: {
+                        Image(systemName: "square.and.arrow.down")
+                    }
+                }
+                
+                ToolbarItemGroup(placement: .topBarTrailing) {
+                    Button {
+                        
+                    }label: {
+                        Text("Edit")
+                    }
+                }
+            }
+            .navigationSearchBar(text: $search,
+                                 scopeSelection: $scopeSelection,
+                                 options: [
+                                    .automaticallyShowsSearchBar: true,
+                                    .obscuresBackgroundDuringPresentation: true,
+                                    .hidesNavigationBarDuringPresentation: true,
+                                    .hidesSearchBarWhenScrolling: false,
+                                    .placeholder: "Search",
+                                    .showsBookmarkButton: true,
+                                    .scopeButtonTitles: ["All", "Missed", "Other"]
+                                 ],
+                                 actions: [
+                                    .onCancelButtonClicked: {
+                                        print("Cancel")
+                                    },
+                                    .onSearchButtonClicked: {
+                                        print("Search")
+                                    },
+                                    .onBookmarkButtonClicked: {
+                                        print("Present Bookmarks")
+                                    }
+                                 ], searchResultsContent: {
+                                     NavigationLink(destination: Text("Destination")) {
+                                         Text("Search Results for \(search) in \(String(scopeSelection))")
+                                     }
+                                 })
         }
     }
 }
